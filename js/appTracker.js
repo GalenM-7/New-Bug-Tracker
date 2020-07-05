fefb88f725a698879f0c22ca6395b356e38f3760
Galen Montrone <gjmontrone@gmail.com>
2020-07-04 20:56:32 -0400

    New Task 1

---
+++ js/appTracker.js
@@ -0,0 +1,10 @@
+let project=[];
+
+
+let taskObject = { 
+    name: document.getElementById("taskName").value,
+    taskAssignment: document.getElementById("taskAssignment").value,
+    deadline: document.getElementById("deadline").value,
+    additonalDates: document.getElementById("additonalDates").value,
+    taskDescription: document.getElementById("taskDescription").value,
+}
