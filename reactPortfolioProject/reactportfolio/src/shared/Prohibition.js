dd2380b9c6a3909f4c5b9f8fa4ba48805e227f89
Galen Montrone <gjmontrone@gmail.com>
2020-09-14 18:59:51 -0400

    first commit react app/card game

---
+++ reactPortfolioProject/reactportfolio/src/shared/Prohibition.js
@@ -0,0 +1,26 @@
+export const prohibitionBootleggers=[
+    {   
+        id:0,
+        name: "Al Capone",
+        earlyYears: "The 100",
+        criminalCareer: "",
+        notorietyRank: "",
+        evilRank: "",
+    },
+    {   
+        id:1,
+        name: "Dutch Schultz",
+        earlyYears: "Harry Potter",
+        criminalCareer:"",
+        notorietyRank:"",
+        evilRank:"",
+    },
+    {
+        id:2,
+        name: "Charles aka Lucky Luciano",
+        earlyYears: "Sons of Anarchy",
+        criminalCareer:"",
+        notorietyRank:"",
+        evilRank:"",
+    },
+]
